0aa8c55300e86b1d5b8a332163722c9d74e11ac3
oleksandr.o.morozov <morozovaleks@ukr.net>
2022-03-31 17:12:46 +0300

    hilt di

--- app/src/main/java/com/example/myreader/MainActivity.kt
+++ app/src/main/java/com/example/myreader/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myreader.ui.theme.MyReaderTheme
 import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
