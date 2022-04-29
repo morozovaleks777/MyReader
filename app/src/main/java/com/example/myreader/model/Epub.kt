7f754fbebeb46e11fa694d6122e298034d7b06e9
oleksandr.o.morozov <morozovaleks@ukr.net>
2022-04-29 15:29:35 +0300

    model classes

---
+++ app/src/main/java/com/example/myreader/model/Epub.kt
@@ -0,0 +1,11 @@
+package com.example.myreader.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Epub(
+    @SerializedName("acsTokenLink")
+    val acsTokenLink: String,
+    @SerializedName("isAvailable")
+    val isAvailable: Boolean
+)
---
+++ app/src/main/java/com/example/myreader/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.myreader.utils
+
+object Constants {
+}
